a43cf458acfae757c33fe396cfb8870bd9178501
Zeemzo <azeemashraf@outlook.com>
2018-09-07 21:36:58 +0530

    heruko test

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,23 @@ import * as apiRoutes from "./routes/routes";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-
-
 // Create a new express application instance
-const app: express.Application = express();
+// const app: express.Application = express();
 // The port the express app will listen on
 const port =process.env.PORT||7000;
 // app.use(bodyParser);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+express()
+.use(bodyParser.urlencoded({ extended: true }))
+.use(bodyParser.json())
 
-app.use( "/api" , apiRoutes.router);
+.use( "/api" , apiRoutes.router)
 
 // Serve the application at the given port
-app.listen(port, () => {
+.listen(port, () => {
     // Success callback
     // tslint:disable-next-line:no-console
     console.log(`Listening at http://localhost:${port}/`);
 });
 
 
-export default app;
+// export default app;

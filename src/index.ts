82330027a1080ada9346f793b85bb2a630dbbb39
Zeemzo <azeemashraf@outlook.com>
2018-09-27 09:15:15 +0530

    corrrrrrsksdjksdf

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,17 @@ dotenv.config();
 const app: express.Application = express();
 // The port the express app will listen on
 const port =process.env.PORT||7000;
-
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+// app.use(cors({
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }));
 
 // app.use(bodyParser);
 // app.use(bodyParser.urlencoded({ extended: true }));
